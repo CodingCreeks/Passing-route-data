588887cd3d163594ca64d5a3211068c676ef2064
CodingCreeks <halappa.manjunatha@gmail.com>
2019-07-23 16:13:26 -0400

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,16 +10,26 @@ import { FormComponent } from './form/form.component';
 //   { path: 'search3', component: FormComponent, data: { name: 'Order search list-3' } },
 //   { path: 'search4', component: FormComponent, data: { name: 'Order search list-4' } }
 // ];
+// const routes: Routes = [
+//   { path: 'details', component: DetailsPageComponent },
+//   {
+//     path: 'search',
+//     children: [
+//       { path: '1', component: FormComponent, data: { name: 'Order search list-1' } },
+//       { path: '2', component: FormComponent, data: { name: 'Order search list-2' } },
+//       { path: '3', component: FormComponent, data: { name: 'Order search list-3' } },
+//       { path: '4', component: FormComponent, data: { name: 'Order search list-4' } }
+
+//     ]
+//   },
+// ];
 const routes: Routes = [
   { path: 'details', component: DetailsPageComponent },
   {
     path: 'search',
     children: [
-      { path: '1', component: FormComponent, data: { name: 'Order search list-1' } },
-      { path: '2', component: FormComponent, data: { name: 'Order search list-2' } },
-      { path: '3', component: FormComponent, data: { name: 'Order search list-3' } },
-      { path: '4', component: FormComponent, data: { name: 'Order search list-4' } }
-
+      { path: '', component: FormComponent },
+      { path: ':id', component: FormComponent },
     ]
   },
 ];
